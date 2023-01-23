25cb40c984ba8e0f2d537c967c9d6ead6b638b98
Vitor Petruz Brina <vitor.p.brina@westrock.com>
2023-01-23 15:59:06 -0300

    Server: Add a fallback to return empty array in completedHabits.

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -61,7 +61,7 @@ export const appRoutes = async (app: FastifyInstance) => {
 
     const completedHabits = day?.dayHabits.map((dayHabit) => {
       return dayHabit.habit_id;
-    });
+    }) ?? [];
 
     return {
       possibleHabits,
